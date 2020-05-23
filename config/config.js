e33618d7cf19def5ab7dbcd59fdb9506e3fcc03d
sanket1995 <sanket.g@boppotechnologies.com>
2020-05-23 19:09:19 +0530

    secomd commit

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 
 
 module.exports={
-  uri:'mongodb+srv://sank09:12345@cluster0-osnok.mongodb.net/test?retryWrites=true&w=majority',
+  //Put your MongoDb atlas connection string in uri
+  uri:'',
   jwtSecret:"myjwt"
 }     
-
